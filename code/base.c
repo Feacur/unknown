3bd6637f7bbc7705c79ebb7afb3d6df4513f6607
feacur <feacur@gmail.com>
2025-08-23 19:38:34 +0200

    add an initial base layer implementation

--- code/base.c
+++ code/base.c
@@ -1,9 +1,120 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 
 #include "base.h"
 
+/*
+@note floating point IEEE 754
+
+| TYPE | BINARY REPRESENTATION                | FORMULA                          | FINITE EXPONENT |
+| f16  | [sign:1][exponent: s5][mantissa:u10] | (1-S*2) * (1 + M/(2^10)) * (2^E) | [  -15 ..   15] |
+| f32  | [sign:1][exponent: s8][mantissa:u23] | (1-S*2) * (1 + M/(2^23)) * (2^E) | [ -127 ..  127] |
+| f64  | [sign:1][exponent:s11][mantissa:u52] | (1-S*2) * (1 + M/(2^52)) * (2^E) | [-1023 .. 1023] |
+
+`exponent` is a "window" between powers of two
+`mantissa` is an "offset" within a said "window"
+
+for `f32` with `E == 0`    "window" is `[1 .. 2)`
+for `f32` with `M == 2^22` "offset" is `0.5`
+  result is `1.5`
+
+for `f16` with `E ==   -16` result is `infinity` if `M == 0`, otherwise `nan` (quiet if highest bit on, signaling in other case)
+for `f32` with `E ==  -128` result is `infinity` if `M == 0`, otherwise `nan` (quiet if highest bit on, signaling in other case)
+for `f64` with `E == -1024` result is `infinity` if `M == 0`, otherwise `nan` (quiet if highest bit on, signaling in other case)
+
+consecutive integer range is [-2^(mantissa width + 1) .. 2^(mantissa width + 1)]
+
+
+
+
+@note coordinate system is left-handed
+
+Y (0, 1, 0) up
+|     
+|  Z (0, 0, 1) forward
+| /   
+|/    
++----X (1, 0, 0) right
+
+X = cross(Y, Z)
+Y = cross(Z, X)
+Z = cross(X, Y)
+
+
+
+
+@note designated initializers looks have nothing to do with ordering
+
+V_rm x M_rm = transpose(M_cm x V_cm)
+M_cm x V_cm = transpose(V_cm x M_cm)
+
+> ROW-MAJOR ORDER means interpreting contiguous memory like this
+                    +---------------+
+                    |x_x x_y x_z x_w| -> axis_x
+                    +---------------+
+                    |y_x y_y y_z y_w| -> axis_y
+                    +---------------+
+                    |z_x z_y z_z z_w| -> axis_z
+                    +---------------+
+                    |w_x w_y w_z w_w| -> offset
+                    +---------------+
+
+                    +---------------+
+                    |x_x x_y x_z x_w|
+                    +---------------+
++---------------+   |y_x y_y y_z y_w|   +---------------+
+|v_x v_y v_z v_w| x +---------------+ = |r_x r_y r_z r_w|
++---------------+   |z_x z_y z_z z_w|   +---------------+
+                    +---------------+
+                    |w_x w_y w_z w_w|
+                    +---------------+
+
+> COLUMN-MAJOR ORDER in its turn means doing that way around
+ axis_x  axis_z
+ |       |
++---------------+
+|x_x|y_x|z_x|w_x|
+|   |   |   |   |
+|x_y|y_y|z_y|w_y|
+|   |   |   |   |
+|x_z|y_z|z_z|w_z|
+|   |   |   |   |
+|x_w|y_w|z_w|w_w|
++---------------+
+     |       |
+     axis_y  offset
+
++---------------+   +---+   +---+
+|x_x|y_x|z_x|w_x|   |v_x|   |r_x|
+|   |   |   |   |   |   |   |   |
+|x_y|y_y|z_y|w_y|   |v_y|   |r_y|
+|   |   |   |   | x |   | = |   |
+|x_z|y_z|z_z|w_z|   |v_z|   |r_z|
+|   |   |   |   |   |   |   |   |
+|x_w|y_w|z_w|w_w|   |v_w|   |r_w|
++---------------+   +---+   +---+
+*/
+
 // ---- ---- ---- ----
-// functions
+// types
+// ---- ---- ---- ----
+
+AssertSize(u8,  1);
+AssertSize(u16, 2);
+AssertSize(u32, 4);
+AssertSize(u64, 8);
+
+AssertSize(s8,  1);
+AssertSize(s16, 2);
+AssertSize(s32, 4);
+AssertSize(s64, 8);
+
+AssertSize(f32, 4);
+AssertSize(f64, 8);
+
+// ---- ---- ---- ----
+// functions, basic
 // ---- ---- ---- ----
 
 char base_get_chr(void) {
@@ -13,6 +124,231 @@ char base_get_chr(void) {
 char * base_get_str(char * buffer, int limit) {
 	return fgets(buffer, limit, stdin);
 }
+
+void mem_zero(void * target, size_t size) {
+	if (target != NULL && size > 0)
+		memset(target, 0, size);
+}
+
+void mem_copy(void const * source, void * target, size_t size) {
+	memcpy(target, source, size);
+}
+
+bool str_equals(char const * v1, char const * v2) {
+	return strcmp(v1, v2) == 0;
+}
+
+size_t align_size(size_t value, size_t align) {
+	size_t const mask = max_size(align, 1) - 1;
+	return (value + mask) & ~mask;
+}
+
+u64 mul_div_u64(u64 value, u64 mul, u64 div) {
+	// @note `value * mul / div` equivalent
+	// but with a tiny overflow protection
+	return (value / div) * mul
+	     + (value % div) * mul / div;
+}
+
+// ---- ---- ---- ----
+// functions, limit
+// ---- ---- ---- ----
+
+u8  min_u8(u8   v1,  u8 v2) { return v1 < v2 ? v1 : v2; }
+u16 min_u16(u16 v1, u16 v2) { return v1 < v2 ? v1 : v2; }
+u32 min_u32(u32 v1, u32 v2) { return v1 < v2 ? v1 : v2; }
+u64 min_u64(u64 v1, u64 v2) { return v1 < v2 ? v1 : v2; }
+
+s8  min_s8(s8   v1,  s8 v2) { return v1 < v2 ? v1 : v2; }
+s16 min_s16(s16 v1, s16 v2) { return v1 < v2 ? v1 : v2; }
+s32 min_s32(s32 v1, s32 v2) { return v1 < v2 ? v1 : v2; }
+s64 min_s64(s64 v1, s64 v2) { return v1 < v2 ? v1 : v2; }
+
+f32 min_f32(f32 v1, f32 v2) { return v1 < v2 ? v1 : v2; }
+f64 min_f64(f64 v1, f64 v2) { return v1 < v2 ? v1 : v2; }
+
+long min_long(long v1, long v2) { return v1 < v2 ? v1 : v2; }
+size_t min_size(size_t v1, size_t v2) { return v1 < v2 ? v1 : v2; }
+
+u8  max_u8(u8   v1,  u8 v2) { return v1 > v2 ? v1 : v2; }
+u16 max_u16(u16 v1, u16 v2) { return v1 > v2 ? v1 : v2; }
+u32 max_u32(u32 v1, u32 v2) { return v1 > v2 ? v1 : v2; }
+u64 max_u64(u64 v1, u64 v2) { return v1 > v2 ? v1 : v2; }
+
+s8  max_s8(s8   v1,  s8 v2) { return v1 > v2 ? v1 : v2; }
+s16 max_s16(s16 v1, s16 v2) { return v1 > v2 ? v1 : v2; }
+s32 max_s32(s32 v1, s32 v2) { return v1 > v2 ? v1 : v2; }
+s64 max_s64(s64 v1, s64 v2) { return v1 > v2 ? v1 : v2; }
+
+f32 max_f32(f32 v1, f32 v2) { return v1 > v2 ? v1 : v2; }
+f64 max_f64(f64 v1, f64 v2) { return v1 > v2 ? v1 : v2; }
+
+long max_long(long v1, long v2) { return v1 > v2 ? v1 : v2; }
+size_t max_size(size_t v1, size_t v2) { return v1 > v2 ? v1 : v2; }
+
+u8  clamp_u8(u8   v,  u8 min,  u8 max) { return v < min ? min : v > max ? max : v; }
+u16 clamp_u16(u16 v, u16 min, u16 max) { return v < min ? min : v > max ? max : v; }
+u32 clamp_u32(u32 v, u32 min, u32 max) { return v < min ? min : v > max ? max : v; }
+u64 clamp_u64(u64 v, u64 min, u64 max) { return v < min ? min : v > max ? max : v; }
+
+s8  clamp_s8(s8   v,  s8 min,  s8 max) { return v < min ? min : v > max ? max : v; }
+s16 clamp_s16(s16 v, s16 min, s16 max) { return v < min ? min : v > max ? max : v; }
+s32 clamp_s32(s32 v, s32 min, s32 max) { return v < min ? min : v > max ? max : v; }
+s64 clamp_s64(s64 v, s64 min, s64 max) { return v < min ? min : v > max ? max : v; }
+
+f32 clamp_f32(f32 v, f32 min, f32 max) { return v < min ? min : v > max ? max : v; }
+f64 clamp_f64(f64 v, f64 min, f64 max) { return v < min ? min : v > max ? max : v; }
+
+long clamp_long(long v, long min, long max) { return v < min ? min : v > max ? max : v; }
+size_t clamp_size(size_t v, size_t min, size_t max) { return v < min ? min : v > max ? max : v; }
+
+// ---- ---- ---- ----
+// functions, array
+// ---- ---- ---- ----
+
+void arr8_append_unique(arr8 * target, u8 value) {
+	for (size_t i = 0; i < target->count; i++)
+		if (target->buffer[i] == value)
+			return;
+	target->buffer[target->count++] = value;
+}
+
+void arr16_append_unique(arr16 * target, u16 value) {
+	for (size_t i = 0; i < target->count; i++)
+		if (target->buffer[i] == value)
+			return;
+	target->buffer[target->count++] = value;
+}
+
+void arr32_append_unique(arr32 * target, u32 value) {
+	for (size_t i = 0; i < target->count; i++)
+		if (target->buffer[i] == value)
+			return;
+	target->buffer[target->count++] = value;
+}
+
+// ---- ---- ---- ----
+// functions, string
+// ---- ---- ---- ----
+
+void str8_append(str8 * target, str8 value) {
+	mem_copy(value.buffer, target->buffer + target->count, value.count);
+	target->count += value.count;
+}
+
+// ---- ---- ---- ----
+// functions, f32 math, vector
+// ---- ---- ---- ----
+
+vec2 vec2_add(vec2 l, vec2 r) { return (vec2){l.x + r.x, l.y + r.y}; }
+vec2 vec2_sub(vec2 l, vec2 r) { return (vec2){l.x - r.x, l.y - r.y}; }
+vec2 vec2_mul(vec2 l, vec2 r) { return (vec2){l.x * r.x, l.y * r.y}; }
+vec2 vec2_div(vec2 l, vec2 r) { return (vec2){l.x / r.x, l.y / r.y}; }
+f32  vec2_dot(vec2 l, vec2 r) { return l.x * r.x + l.y * r.y; }
+
+vec3 vec3_add(vec3 l, vec3 r) { return (vec3){l.x + r.x, l.y + r.y, l.z + r.z}; }
+vec3 vec3_sub(vec3 l, vec3 r) { return (vec3){l.x - r.x, l.y - r.y, l.z - r.z}; }
+vec3 vec3_mul(vec3 l, vec3 r) { return (vec3){l.x * r.x, l.y * r.y, l.z * r.z}; }
+vec3 vec3_div(vec3 l, vec3 r) { return (vec3){l.x / r.x, l.y / r.y, l.z / r.z}; }
+f32  vec3_dot(vec3 l, vec3 r) { return l.x * r.x + l.y * r.y + l.z * r.z; }
+
+vec4 vec4_add(vec4 l, vec4 r) { return (vec4){l.x + r.x, l.y + r.y, l.z + r.z, l.w + r.w}; }
+vec4 vec4_sub(vec4 l, vec4 r) { return (vec4){l.x - r.x, l.y - r.y, l.z - r.z, l.w - r.w}; }
+vec4 vec4_mul(vec4 l, vec4 r) { return (vec4){l.x * r.x, l.y * r.y, l.z * r.z, l.w * r.w}; }
+vec4 vec4_div(vec4 l, vec4 r) { return (vec4){l.x / r.x, l.y / r.y, l.z / r.z, l.w / r.w}; }
+f32  vec4_dot(vec4 l, vec4 r) { return l.x * r.x + l.y * r.y + l.z * r.z + l.w * r.w; }
+
+f32  vec2_crs(vec2 l, vec2 r) { return l.x * r.y - l.y * r.x; }
+vec3 vec3_crs(vec3 l, vec3 r) { return (vec3){
+	l.y * r.z - l.z * r.y,
+	l.z * r.x - l.x * r.z,
+	l.x * r.y - l.y * r.x,
+}; }
+
+// ---- ---- ---- ----
+// functions, s32 math, matrix
+// ---- ---- ---- ----
+
+vec2 mat2_mul_vec(mat2 l, vec2 r) { return (vec2){
+	vec2_dot((vec2){l.x.x, l.y.x}, r),
+	vec2_dot((vec2){l.x.y, l.y.y}, r),
+}; }
+
+mat2 mat2_mul_mat(mat2 l, mat2 r) { return (mat2){
+	mat2_mul_vec(l, r.x),
+	mat2_mul_vec(l, r.y),
+}; }
+
+vec3 mat3_mul_vec(mat3 l, vec3 r) { return (vec3){
+	vec3_dot((vec3){l.x.x, l.y.x, l.z.x}, r),
+	vec3_dot((vec3){l.x.y, l.y.y, l.z.y}, r),
+	vec3_dot((vec3){l.x.z, l.y.z, l.z.z}, r),
+}; }
+
+mat3 mat3_mul_mat(mat3 l, mat3 r) { return (mat3){
+	mat3_mul_vec(l, r.x),
+	mat3_mul_vec(l, r.y),
+	mat3_mul_vec(l, r.z),
+}; }
+
+vec4 mat4_mul_vec(mat4 l, vec4 r) { return (vec4){
+	vec4_dot((vec4){l.x.x, l.y.x, l.z.x, l.w.x}, r),
+	vec4_dot((vec4){l.x.y, l.y.y, l.z.y, l.w.y}, r),
+	vec4_dot((vec4){l.x.z, l.y.z, l.z.z, l.w.z}, r),
+	vec4_dot((vec4){l.x.w, l.y.w, l.z.w, l.w.w}, r),
+}; }
+
+mat4 mat4_mul_mat(mat4 l, mat4 r) { return (mat4){
+	mat4_mul_vec(l, r.x),
+	mat4_mul_vec(l, r.y),
+	mat4_mul_vec(l, r.z),
+	mat4_mul_vec(l, r.w),
+}; }
+
+// ---- ---- ---- ----
+// functions, s32 math, vector
+// ---- ---- ---- ----
+
+vec2i vec2i_add(vec2i l, vec2i r) { return (vec2i){l.x + r.x, l.y + r.y}; }
+vec2i vec2i_sub(vec2i l, vec2i r) { return (vec2i){l.x - r.x, l.y - r.y}; }
+vec2i vec2i_mul(vec2i l, vec2i r) { return (vec2i){l.x * r.x, l.y * r.y}; }
+vec2i vec2i_div(vec2i l, vec2i r) { return (vec2i){l.x / r.x, l.y / r.y}; }
+s32   vec2i_dot(vec2i l, vec2i r) { return l.x * r.x + l.y * r.y; }
+
+vec3i vec3i_add(vec3i l, vec3i r) { return (vec3i){l.x + r.x, l.y + r.y, l.z + r.z}; }
+vec3i vec3i_sub(vec3i l, vec3i r) { return (vec3i){l.x - r.x, l.y - r.y, l.z - r.z}; }
+vec3i vec3i_mul(vec3i l, vec3i r) { return (vec3i){l.x * r.x, l.y * r.y, l.z * r.z}; }
+vec3i vec3i_div(vec3i l, vec3i r) { return (vec3i){l.x / r.x, l.y / r.y, l.z / r.z}; }
+s32   vec3i_dot(vec3i l, vec3i r) { return l.x * r.x + l.y * r.y + l.z * r.z; }
+
+vec4i vec4i_add(vec4i l, vec4i r) { return (vec4i){l.x + r.x, l.y + r.y, l.z + r.z, l.w + r.w}; }
+vec4i vec4i_sub(vec4i l, vec4i r) { return (vec4i){l.x - r.x, l.y - r.y, l.z - r.z, l.w - r.w}; }
+vec4i vec4i_mul(vec4i l, vec4i r) { return (vec4i){l.x * r.x, l.y * r.y, l.z * r.z, l.w * r.w}; }
+vec4i vec4i_div(vec4i l, vec4i r) { return (vec4i){l.x / r.x, l.y / r.y, l.z / r.z, l.w / r.w}; }
+s32   vec4i_dot(vec4i l, vec4i r) { return l.x * r.x + l.y * r.y + l.z * r.z + l.w * r.w; }
+
+s32   vec2i_crs(vec2i l, vec2i r) { return l.x * r.y - l.y * r.x; }
+vec3i vec3i_crs(vec3i l, vec3i r) { return (vec3i){
+	l.y * r.z - l.z * r.y,
+	l.z * r.x - l.x * r.z,
+	l.x * r.y - l.y * r.x,
+}; }
+
+// ---- ---- ---- ----
+// constants
+// ---- ---- ---- ----
+
+bits32 const BITS_F32_SEQ  = ((bits32){.as_u = 0x4b800000u}); // 2^24
+bits32 const BITS_F32_LIM  = ((bits32){.as_u = 0x7f7fffffu}); // 2^127 * [1 .. 2)
+bits32 const BITS_F32_INF  = ((bits32){.as_u = 0x7f800000u}); // 2^128
+bits32 const BITS_F32_QNAN = ((bits32){.as_u = 0x7fc00000u}); // 2^128 * (1 .. 2)
+bits32 const BITS_F32_SNAN = ((bits32){.as_u = 0x7fa00000u}); // 2^128 * (1 .. 2)
+
+bits64 const BITS_F64_SEQ  = ((bits64){.as_u = 0x4340000000000000ull}); // 2^53
+bits64 const BITS_F64_LIM  = ((bits64){.as_u = 0x7fefffffffffffffull}); // 2^1023 * [1 .. 2)
+bits64 const BITS_F64_INF  = ((bits64){.as_u = 0x7ff0000000000000ull}); // 2^1024
+bits64 const BITS_F64_QNAN = ((bits64){.as_u = 0x7ff8000000000000ull}); // 2^1024 * (1 .. 2)
+bits64 const BITS_F64_SNAN = ((bits64){.as_u = 0x7ff4000000000000ull}); // 2^1024 * (1 .. 2)
 
 // ---- ---- ---- ----
 // formatting
@@ -24,26 +360,53 @@ char * base_get_str(char * buffer, int limit) {
 # include <stb/stb_sprintf.h>
 #include "_internal/warnings_pop.h"
 
-
-struct Fmt_Ctx {
-	char buffer[STB_SPRINTF_MIN];
+struct Fmt_Print_Ctx {
+	char scratch[STB_SPRINTF_MIN];
 };
 
 AttrFileLocal()
-char * fmt_print_write_chunk(char const * buf, void * user, int len) {
-	struct Fmt_Ctx * ctx = user;
-	if (len > 0) {
-		fwrite(buf, sizeof(*buf), len, stdout);
+char * fmt_print_write(char const * input, void * user, int length) {
+	struct Fmt_Print_Ctx * ctx = user;
+	if (length > 0) {
+		fwrite(input, sizeof(*input), length, stdout);
 	}
-	return ctx->buffer;
+	return ctx->scratch;
 }
 
-void fmt_print(char * fmt, ...) {
+uint32_t fmt_print(char * fmt, ...) {
 	va_list args;
 	va_start(args, fmt);
 
-	struct Fmt_Ctx ctx;
-	stbsp_vsprintfcb(fmt_print_write_chunk, &ctx, ctx.buffer, fmt, args);
+	struct Fmt_Print_Ctx ctx;
+	int const written = stbsp_vsprintfcb(fmt_print_write, &ctx, ctx.scratch, fmt, args);
 
 	va_end(args);
+	return written;
+}
+
+struct Fmt_Buffer_Ctx {
+	char scratch[STB_SPRINTF_MIN];
+	char * output;
+};
+
+AttrFileLocal()
+char * fmt_buffer_write(char const * input, void * user, int length) {
+	struct Fmt_Buffer_Ctx * ctx = user;
+	if (length > 0) {
+		mem_copy(ctx->scratch, ctx->output, length);
+		ctx->output += length;
+	}
+	return ctx->scratch;
+}
+
+AttrPrint(2, 3)
+uint32_t fmt_buffer(char * out_buffer, char * fmt, ...) {
+	va_list args;
+	va_start(args, fmt);
+
+	struct Fmt_Buffer_Ctx ctx = {.output = out_buffer};
+	int const written = stbsp_vsprintfcb(fmt_buffer_write, &ctx, ctx.scratch, fmt, args);
+
+	va_end(args);
+	return written;
 }
--- code/base.h
+++ code/base.h
@@ -1,14 +1,326 @@
 #if !defined (UKWN_BASE_H)
 #define UKWN_BASE_H
 
+#include <stdbool.h> // IWYU pragma: export
+#include <stdint.h> // IWYU pragma: export
+#include <stddef.h> // IWYU pragma: export
+
 #include "_project.h" // IWYU pragma: export
 
 // ---- ---- ---- ----
-// functions
+// types, basic
+// ---- ---- ---- ----
+
+typedef uint8_t   u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t    s8;
+typedef int16_t  s16;
+typedef int32_t  s32;
+typedef int64_t  s64;
+
+typedef float    f32;
+typedef double   f64;
+
+// ---- ---- ---- ----
+// types, bits
+// ---- ---- ---- ----
+
+typedef union Bits32 bits32;
+union Bits32 {
+	u32 as_u;
+	s32 as_s;
+	f32 as_f;
+};
+
+typedef union Bits64 bits64;
+union Bits64 {
+	u64 as_u;
+	s64 as_s;
+	f32 as_f;
+};
+
+// ---- ---- ---- ----
+// types, array
+// ---- ---- ---- ----
+
+typedef struct Array_U8 arr8;
+struct Array_U8 {
+	size_t capacity;
+	size_t count;
+	u8 * buffer;
+};
+
+typedef struct Array_U16 arr16;
+struct Array_U16 {
+	size_t capacity;
+	size_t count;
+	u16 * buffer;
+};
+
+typedef struct Array_U32 arr32;
+struct Array_U32 {
+	size_t capacity;
+	size_t count;
+	u32 * buffer;
+};
+
+// ---- ---- ---- ----
+// types, string
+// ---- ---- ---- ----
+
+typedef struct String_U8 str8;
+struct String_U8 {
+	size_t count;
+	u8 * buffer;
+};
+
+typedef struct String_U16 str16;
+struct String_U16 {
+	size_t count;
+	u16 * buffer;
+};
+
+typedef struct String_U32 str32;
+struct String_U32 {
+	size_t count;
+	u32 * buffer;
+};
+
+// ---- ---- ---- ----
+// types, f32 math
+// ---- ---- ---- ----
+
+typedef struct Vector2_F32 vec2;
+struct Vector2_F32 {
+	f32 x, y;
+};
+
+typedef struct Vector3_F32 vec3;
+struct Vector3_F32 {
+	f32 x, y, z;
+};
+
+typedef struct Vector4_F32 vec4;
+struct Vector4_F32 {
+	f32 x, y, z, w;
+};
+
+typedef struct Matrix2_F32 mat2;
+struct Matrix2_F32 {
+	vec2 x, y;
+};
+
+typedef struct Matrix3_F32 mat3;
+struct Matrix3_F32 {
+	vec3 x, y, z;
+};
+
+typedef struct Matrix4_F32 mat4;
+struct Matrix4_F32 {
+	vec4 x, y, z, w;
+};
+
+// ---- ---- ---- ----
+// types, s32 math
+// ---- ---- ---- ----
+
+typedef struct Vector2_S32 vec2i;
+struct Vector2_S32 {
+	s32 x, y;
+};
+
+typedef struct Vector3_S32 vec3i;
+struct Vector3_S32 {
+	s32 x, y, z;
+};
+
+typedef struct Vector4_S32 vec4i;
+struct Vector4_S32 {
+	s32 x, y, z, w;
+};
+
+// ---- ---- ---- ----
+// functions, basic
 // ---- ---- ---- ----
 
 char   base_get_chr(void);
 char * base_get_str(char * buffer, int limit);
+
+void mem_zero(void * target, size_t size);
+void mem_copy(void const * source, void * target, size_t size);
+bool str_equals(char const * v1, char const * v2);
+
+size_t align_size(size_t value, size_t align);
+
+u64 mul_div_u64(u64 value, u64 mul, u64 div);
+
+// ---- ---- ---- ----
+// functions, limit
+// ---- ---- ---- ----
+
+u8  min_u8(u8   v1,  u8 v2);
+u16 min_u16(u16 v1, u16 v2);
+u32 min_u32(u32 v1, u32 v2);
+u64 min_u64(u64 v1, u64 v2);
+
+s8  min_s8(s8   v1,  s8 v2);
+s16 min_s16(s16 v1, s16 v2);
+s32 min_s32(s32 v1, s32 v2);
+s64 min_s64(s64 v1, s64 v2);
+
+f32 min_f32(f32 v1, f32 v2);
+f64 min_f64(f64 v1, f64 v2);
+
+long min_long(long v1, long v2);
+size_t min_size(size_t v1, size_t v2);
+
+u8  max_u8(u8   v1,  u8 v2);
+u16 max_u16(u16 v1, u16 v2);
+u32 max_u32(u32 v1, u32 v2);
+u64 max_u64(u64 v1, u64 v2);
+
+s8  max_s8(s8   v1,  s8 v2);
+s16 max_s16(s16 v1, s16 v2);
+s32 max_s32(s32 v1, s32 v2);
+s64 max_s64(s64 v1, s64 v2);
+
+f32 max_f32(f32 v1, f32 v2);
+f64 max_f64(f64 v1, f64 v2);
+
+long max_long(long v1, long v2);
+size_t max_size(size_t v1, size_t v2);
+
+u8  clamp_u8(u8   v,  u8 min,  u8 max);
+u16 clamp_u16(u16 v, u16 min, u16 max);
+u32 clamp_u32(u32 v, u32 min, u32 max);
+u64 clamp_u64(u64 v, u64 min, u64 max);
+
+s8  clamp_s8(s8   v,  s8 min,  s8 max);
+s16 clamp_s16(s16 v, s16 min, s16 max);
+s32 clamp_s32(s32 v, s32 min, s32 max);
+s64 clamp_s64(s64 v, s64 min, s64 max);
+
+f32 clamp_f32(f32 v, f32 min, f32 max);
+f64 clamp_f64(f64 v, f64 min, f64 max);
+
+long clamp_long(long v, long min, long max);
+size_t clamp_size(size_t v, size_t min, size_t max);
+
+// ---- ---- ---- ----
+// functions, array
+// ---- ---- ---- ----
+
+void arr8_append_unique(arr8 * target, u8 value);
+void arr16_append_unique(arr16 * target, u16 value);
+void arr32_append_unique(arr32 * target, u32 value);
+
+// ---- ---- ---- ----
+// functions, string
+// ---- ---- ---- ----
+
+void str8_append(str8 * target, str8 value);
+
+// ---- ---- ---- ----
+// functions, f32 math, vector
+// ---- ---- ---- ----
+
+vec2 vec2_add(vec2 l, vec2 r);
+vec2 vec2_sub(vec2 l, vec2 r);
+vec2 vec2_mul(vec2 l, vec2 r);
+vec2 vec2_div(vec2 l, vec2 r);
+f32  vec2_dot(vec2 l, vec2 r);
+
+vec3 vec3_add(vec3 l, vec3 r);
+vec3 vec3_sub(vec3 l, vec3 r);
+vec3 vec3_mul(vec3 l, vec3 r);
+vec3 vec3_div(vec3 l, vec3 r);
+f32  vec3_dot(vec3 l, vec3 r);
+
+vec4 vec4_add(vec4 l, vec4 r);
+vec4 vec4_sub(vec4 l, vec4 r);
+vec4 vec4_mul(vec4 l, vec4 r);
+vec4 vec4_div(vec4 l, vec4 r);
+f32  vec4_dot(vec4 l, vec4 r);
+
+f32  vec2_crs(vec2 l, vec2 r);
+vec3 vec3_crs(vec3 l, vec3 r);
+
+// ---- ---- ---- ----
+// functions, f32 math, matrix
+// ---- ---- ---- ----
+
+vec2 mat2_mul_vec(mat2 l, vec2 r);
+mat2 mat2_mul_mat(mat2 l, mat2 r);
+
+vec3 mat3_mul_vec(mat3 l, vec3 r);
+mat3 mat3_mul_mat(mat3 l, mat3 r);
+
+vec4 mat4_mul_vec(mat4 l, vec4 r);
+mat4 mat4_mul_mat(mat4 l, mat4 r);
+
+// ---- ---- ---- ----
+// functions, s32 math, vector
+// ---- ---- ---- ----
+
+vec2i vec2i_add(vec2i l, vec2i r);
+vec2i vec2i_sub(vec2i l, vec2i r);
+vec2i vec2i_mul(vec2i l, vec2i r);
+vec2i vec2i_div(vec2i l, vec2i r);
+s32   vec2i_dot(vec2i l, vec2i r);
+
+vec3i vec3i_add(vec3i l, vec3i r);
+vec3i vec3i_sub(vec3i l, vec3i r);
+vec3i vec3i_mul(vec3i l, vec3i r);
+vec3i vec3i_div(vec3i l, vec3i r);
+s32   vec3i_dot(vec3i l, vec3i r);
+
+vec4i vec4i_add(vec4i l, vec4i r);
+vec4i vec4i_sub(vec4i l, vec4i r);
+vec4i vec4i_mul(vec4i l, vec4i r);
+vec4i vec4i_div(vec4i l, vec4i r);
+s32   vec4i_dot(vec4i l, vec4i r);
+
+s32   vec2i_crs(vec2i l, vec2i r);
+vec3i vec3i_crs(vec3i l, vec3i r);
+
+// ---- ---- ---- ----
+// macros
+// ---- ---- ---- ----
+
+#define StrToken(token) #token
+#define StrMacro(macro) StrToken(macro)
+
+#define CatToken(a, b) a ## b
+#define CatMacro(a, b) CatToken(a, b)
+
+#define ArrayCount(array) (sizeof(array) / sizeof((array)[0]))
+#define FieldSize(type, name) sizeof(((type *)0)->name)
+
+#if defined(__clang__)
+# define AlignOf(type) __alignof__(type)
+#elif defined(_MSC_VER)
+# define AlignOf(type) alignof(type)
+#else
+# error not implemented
+#endif
+
+#define AsMillis(seconds) (u64)((seconds) * (u64)1000)
+#define AsMicros(seconds) (u64)((seconds) * (u64)1000000)
+#define AsNanos(seconds)  (u64)((seconds) * (u64)1000000000)
+
+#define KB(value) ((u64)(value) << 10)
+#define MB(value) ((u64)(value) << 20)
+#define GB(value) ((u64)(value) << 30)
+#define TB(value) ((u64)(value) << 40)
+
+#define str8_lit(value)                \
+/**/(struct String_U8){                \
+/**/    .count = sizeof("" value) - 1, \
+/**/    .buffer = (u8*)(value),        \
+/**/}                                  \
 
 // ---- ---- ---- ----
 // attributes
@@ -16,8 +328,25 @@ char * base_get_str(char * buffer, int limit);
 
 #define AttrFuncLocal() static
 #define AttrFileLocal() static
+#define AttrExternal() extern
 
-#if defined(__clang__)
+#if defined (__has_attribute) && __has_attribute(fallthrough)
+// @note it's a clang / GCC feature
+# define AttrFallthrough() __attribute__((fallthrough))
+#else
+# error not implemented
+#endif
+
+#if defined (__clang__) || __has_attribute(__GNUC__)
+# define AttrThreadLocal() __thread
+#elif defined(_MSC_VER)
+# define AttrThreadLocal() __declspec(thread)
+#else
+# error not implemented
+#endif
+
+#if defined (__has_attribute) && __has_attribute(format)
+// @note it's a clang / GCC feature
 # define AttrPrint(fmt, args)                \
 /**/__attribute__((format(printf,fmt,args))) \
 
@@ -26,10 +355,97 @@ char * base_get_str(char * buffer, int limit);
 #endif
 
 // ---- ---- ---- ----
+// constants
+// ---- ---- ---- ----
+
+AttrExternal() const bits32 BITS_F32_SEQ;
+AttrExternal() const bits32 BITS_F32_LIM;
+AttrExternal() const bits32 BITS_F32_INF;
+AttrExternal() const bits32 BITS_F32_QNAN;
+AttrExternal() const bits32 BITS_F32_SNAN;
+
+AttrExternal() const bits64 BITS_F64_SEQ;
+AttrExternal() const bits64 BITS_F64_LIM;
+AttrExternal() const bits64 BITS_F64_INF;
+AttrExternal() const bits64 BITS_F64_QNAN;
+AttrExternal() const bits64 BITS_F64_SNAN;
+
+#define F32_SEQ  BITS_F32_SEQ.as_f
+#define F32_LIM  BITS_F32_LIM.as_f
+#define F32_INF  BITS_F32_INF.as_f
+#define F32_QNAN BITS_F32_QNAN.as_f
+#define F32_SNAN BITS_F32_SNAN.as_f
+
+#define F64_SEQ  BITS_F64_SEQ.as_f
+#define F64_LIM  BITS_F64_LIM.as_f
+#define F64_INF  BITS_F64_INF.as_f
+#define F64_QNAN BITS_F64_QNAN.as_f
+#define F64_SNAN BITS_F64_SNAN.as_f
+
+// ---- ---- ---- ----
 // formatting
 // ---- ---- ---- ----
 
 AttrPrint(1, 2)
-void fmt_print(char * fmt, ...);
+uint32_t fmt_print(char * fmt, ...);
+
+AttrPrint(2, 3)
+uint32_t fmt_buffer(char * out_buffer, char * fmt, ...);
+
+// ---- ---- ---- ----
+// debugging
+// ---- ---- ---- ----
+
+#define FileLine __FILE__ ":" StrMacro(__LINE__)
+
+#if defined(__has_builtin) && __has_builtin(__builtin_debugtrap)
+// @note it's a clang feature
+# define Breakpoint() __builtin_debugtrap()
+#elif defined(__has_builtin) && __has_builtin(__builtin_trap)
+// @note it's a GCC feature
+# define Breakpoint() __builtin_trap()
+#elif defined(_MSC_VER)
+# define Breakpoint() __debugbreak()
+#elif defined(_WIN32)
+void DebugBreak();
+# define Breakpoint() DebugBreak()
+#elif defined(__x86_64__)
+# define Breakpoint() __asm__ volatile ("int3")
+#else
+#error not implemented
+#endif
+
+#define AssertStatic(condition) \
+/**/typedef char assert_static  \
+/**/[(condition) ? 1 : -1]      \
+
+#define AssertSize(type, size)            \
+/**/AssertStatic(sizeof(type) == (size)); \
+
+#if BUILD_DEBUG == BUILD_DEBUG_ENABLE
+# define Assert(condition, msg, ...)           \
+/**/do {                                       \
+/**/    if (!(condition)) {                    \
+/**/        fmt_print("" msg, ## __VA_ARGS__); \
+/**/        fmt_print("  @ " FileLine "\n");   \
+/**/        Breakpoint();                      \
+/**/    }                                      \
+/**/} while (0)                                \
+
+#else
+# define Assert(condition, msg, ...)               \
+/**/(void)(0, (condition), "" msg, ## __VA_ARGS__) \
+
+#endif
+
+#if BUILD_DEBUG == BUILD_DEBUG_ENABLE
+# define DbgPrint(fmt, ...)           \
+/**/fmt_print("" fmt, ## __VA_ARGS__) \
+
+#else
+# define DbgPrint(fmt, ...)           \
+/**/(void)(0, "" fmt, ## __VA_ARGS__) \
+
+#endif
 
 #endif
