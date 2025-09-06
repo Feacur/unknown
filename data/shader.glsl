25b96384249201ea4701e4fba47ff4ac7940e105
feacur <feacur@gmail.com>
2025-09-06 02:00:48 +0200

    introduce uniform buffers

--- data/shader.glsl
+++ data/shader.glsl
@@ -13,8 +13,25 @@ layout(location = 1) in vec3 in_color;
 
 layout(location = 0) out vec3 fragment_color;
 
+// layout(binding = 0) uniform UModel {
+// 	mat4 transform;
+// } u_model;
+
+// layout(binding = 1) uniform UCamera {
+// 	mat4 transform;
+// 	mat4 projection;
+// 	mat4 premultiplied;
+// } u_camera;
+
+layout(binding = 0) uniform UData {
+	mat4 model;
+	mat4 view;
+	mat4 projection;
+} u_data;
+
 void main() {
-	gl_Position = vec4(in_position, 0.0, 1.0);
+	// gl_Position = u_camera.premultiplied * u_model.transform * vec4(in_position, 0.0, 1.0);
+	gl_Position = u_data.projection * u_data.view * u_data.model * vec4(in_position, 0.0, 1.0);
 	fragment_color = in_color;
 }
 #endif
